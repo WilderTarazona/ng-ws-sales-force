e0945efae5b2408977599405bbd9c8fe27bf229a
GraceFT <gracefctorres@gmail.com>
2019-11-15 12:32:51 -0500

    fix en login

--- projects/portal/src/app/features/main/core/components/components.module.ts
+++ projects/portal/src/app/features/main/core/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+// import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { GainComponent } from './cards/gain/gain.component';
 import { InspiraComponent } from './cards/inspira/inspira.component';
