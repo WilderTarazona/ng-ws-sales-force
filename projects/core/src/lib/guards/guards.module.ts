fb82b1ad1f9820f1a6b81036192f5bf0e36b5754
WilderTarazona <wtarazonac@gmail.com>
2019-11-15 13:26:31 -0500

    Se crean los guards

--- projects/core/src/lib/guards/guards.module.ts
+++ projects/core/src/lib/guards/guards.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import {RoleGuard} from './role.guard';
 import {RouterModule} from '@angular/router';
+import {RoleGuard} from './role.guard';
+import {AuthGuard} from './auth.guard';
 
 @NgModule({
   imports: [
     RouterModule
   ],
-  providers: [
-    RoleGuard
-  ]
+  providers: [ RoleGuard, AuthGuard ]
 })
 export class PortalGuardsModule { }
